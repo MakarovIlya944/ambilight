b08433bd7f0caee71053cb2897b893a5a91197bf
Ilya Makarov <makarovilya944@yandex.ru>
2022-06-30 10:49:57 +0300

    Init

---
+++ Form1.Designer.cs
@@ -0,0 +1,252 @@
+﻿namespace ambilightsharp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.comlist = new System.Windows.Forms.ComboBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.testbutton = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.color = new System.Windows.Forms.CheckBox();
+            this.colour = new System.Windows.Forms.CheckBox();
+            this.heightled = new System.Windows.Forms.NumericUpDown();
+            this.refresh = new System.Windows.Forms.CheckBox();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.traymenu = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.onOffToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.heightled)).BeginInit();
+            this.traymenu.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // comlist
+            // 
+            this.comlist.FormattingEnabled = true;
+            this.comlist.Items.AddRange(new object[] {
+            "COM1",
+            "COM2",
+            "COM3",
+            "COM4",
+            "COM5",
+            "COM6",
+            "COM7",
+            "COM8",
+            "COM9",
+            "COM10",
+            "COM11"});
+            this.comlist.Location = new System.Drawing.Point(12, 38);
+            this.comlist.Name = "comlist";
+            this.comlist.Size = new System.Drawing.Size(121, 21);
+            this.comlist.TabIndex = 0;
+            this.comlist.Text = "COM4";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(13, 13);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(59, 17);
+            this.checkBox1.TabIndex = 1;
+            this.checkBox1.Text = "On/Off";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(92, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(59, 20);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Text = "92";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(157, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(79, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "кол-во диодов";
+            // 
+            // testbutton
+            // 
+            this.testbutton.Location = new System.Drawing.Point(141, 38);
+            this.testbutton.Name = "testbutton";
+            this.testbutton.Size = new System.Drawing.Size(95, 23);
+            this.testbutton.TabIndex = 4;
+            this.testbutton.Text = "Предпросмотр";
+            this.testbutton.UseVisualStyleBackColor = true;
+            this.testbutton.Click += new System.EventHandler(this.testbutton_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(13, 78);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1400, 600);
+            this.panel1.TabIndex = 6;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // color
+            // 
+            this.color.AutoSize = true;
+            this.color.Checked = true;
+            this.color.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.color.Location = new System.Drawing.Point(242, 40);
+            this.color.Name = "color";
+            this.color.Size = new System.Drawing.Size(62, 17);
+            this.color.TabIndex = 7;
+            this.color.Text = "Цифры";
+            this.color.UseVisualStyleBackColor = true;
+            this.color.CheckedChanged += new System.EventHandler(this.color_CheckedChanged);
+            // 
+            // colour
+            // 
+            this.colour.AutoSize = true;
+            this.colour.Checked = true;
+            this.colour.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.colour.Location = new System.Drawing.Point(310, 40);
+            this.colour.Name = "colour";
+            this.colour.Size = new System.Drawing.Size(51, 17);
+            this.colour.TabIndex = 8;
+            this.colour.Text = "Цвет";
+            this.colour.UseVisualStyleBackColor = true;
+            this.colour.CheckedChanged += new System.EventHandler(this.colour_CheckedChanged);
+            // 
+            // heightled
+            // 
+            this.heightled.Increment = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.heightled.Location = new System.Drawing.Point(454, 37);
+            this.heightled.Maximum = new decimal(new int[] {
+            500,
+            0,
+            0,
+            0});
+            this.heightled.Minimum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.heightled.Name = "heightled";
+            this.heightled.Size = new System.Drawing.Size(73, 20);
+            this.heightled.TabIndex = 9;
+            this.heightled.Value = new decimal(new int[] {
+            100,
+            0,
+            0,
+            0});
+            this.heightled.ValueChanged += new System.EventHandler(this.heightled_ValueChanged);
+            // 
+            // refresh
+            // 
+            this.refresh.AutoSize = true;
+            this.refresh.Location = new System.Drawing.Point(367, 40);
+            this.refresh.Name = "refresh";
+            this.refresh.Size = new System.Drawing.Size(81, 17);
+            this.refresh.TabIndex = 10;
+            this.refresh.Text = "Обновлять";
+            this.refresh.UseVisualStyleBackColor = true;
+            this.refresh.CheckedChanged += new System.EventHandler(this.refresh_CheckedChanged);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.ContextMenuStrip = this.traymenu;
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "Ambilight";
+            this.notifyIcon1.Visible = true;
+            // 
+            // traymenu
+            // 
+            this.traymenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.onOffToolStripMenuItem});
+            this.traymenu.Name = "traymenu";
+            this.traymenu.Size = new System.Drawing.Size(181, 48);
+            // 
+            // onOffToolStripMenuItem
+            // 
+            this.onOffToolStripMenuItem.Name = "onOffToolStripMenuItem";
+            this.onOffToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.onOffToolStripMenuItem.Text = "On\\Off";
+            this.onOffToolStripMenuItem.Click += new System.EventHandler(this.onOffToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1425, 690);
+            this.Controls.Add(this.refresh);
+            this.Controls.Add(this.heightled);
+            this.Controls.Add(this.colour);
+            this.Controls.Add(this.color);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.testbutton);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.comlist);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Ambilight";
+            ((System.ComponentModel.ISupportInitialize)(this.heightled)).EndInit();
+            this.traymenu.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comlist;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button testbutton;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.CheckBox color;
+        private System.Windows.Forms.CheckBox colour;
+        private System.Windows.Forms.NumericUpDown heightled;
+        private System.Windows.Forms.CheckBox refresh;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.ContextMenuStrip traymenu;
+        private System.Windows.Forms.ToolStripMenuItem onOffToolStripMenuItem;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,340 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+using System.Threading;
+using System.Drawing.Imaging;
+
+namespace ambilightsharp
+{
+    public partial class Form1 : Form
+    {
+        static SerialPort _serialPort;
+        static bool _continue;
+        static int LedNumber = 92;
+        static int LedHeight = 100;
+        // 2     60      2
+        //3               2
+        //26             31
+        //2               2
+        //2  28   4   30  2
+        // sm
+        //-------------------
+        static double[] metrics = new double[] { 2, 30, 4, 28, 2, 
+            2, 28, 2, 
+            3, 59, 3,
+            2, 31, 2 };
+        static double[] leds = new double[] { 16, 16, 14, 32, 14 };
+
+        Thread updateLed;
+
+        static int BaudRate = 38400;
+
+        static bool drawNumber = true;
+        static bool drawColour = true;
+        public Form1()
+        {
+            InitializeComponent();
+            timer1.Interval = 1000;
+        }
+
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked)
+            {
+                _serialPort = new SerialPort(comlist.Text);
+                _serialPort.BaudRate = BaudRate;
+                _serialPort.Open();
+                _continue = true;
+                updateLed = new Thread(UpdateLed);
+                updateLed.Start();
+            }
+            else
+            {
+                _continue = false;
+                updateLed.Abort();
+                _serialPort.Close();
+            }
+        }
+
+        private void ProcessOnOff()
+        {
+            if (checkBox1.Checked)
+            {
+                _serialPort = new SerialPort(comlist.Text);
+                _serialPort.BaudRate = BaudRate;
+                _serialPort.Open();
+                _continue = true;
+                updateLed = new Thread(UpdateLed);
+                updateLed.Start();
+            }
+            else
+            {
+                _continue = false;
+                updateLed.Abort();
+                _serialPort.Close();
+            }
+        }
+
+        static private void UpdateLed()
+        {
+            char[] buff = new char[1];
+            while (_continue)
+            {
+                try
+                {
+                    _serialPort.Write("B");
+                    _serialPort.Write(GetBorder(), 0, 92 * 3);
+                    _serialPort.Read(buff, 0, 1);
+                    while (buff[0] != 'E')
+                        _serialPort.Read(buff, 0, 1);
+                }
+                catch (ThreadAbortException ex)
+                {
+                    return;
+                }
+                
+            }
+        }
+
+
+        //       32      
+        //14             14
+        //   16       16
+        // diods
+        //------------------
+        //         3   
+        //2                4
+        //1                0
+        static private byte[] GetBorder()
+        {
+            Rectangle bounds = Screen.GetBounds(Point.Empty);
+            int j = 0;
+            byte[] result = new byte[LedNumber*3+j];
+            double wid = bounds.Width / 64.0, hei = bounds.Height / 33.0;
+            using (Bitmap bitmap = new Bitmap(bounds.Width, bounds.Height))
+            {
+                using (Graphics g = Graphics.FromImage(bitmap))
+                {
+                    g.CopyFromScreen(Point.Empty, Point.Empty, bounds.Size);
+                }
+
+                
+                int ledWidth = (int)(wid * metrics[1] / leds[0]);
+                int ledHeight = (int)(hei * metrics[6] / leds[0]);
+                int offset = (int)(metrics[0] * wid);
+                for (int i = 0; i < leds[0]; i++)
+                {
+                    var res = AvgRect(bitmap, bounds.Width - offset - i * ledWidth, bounds.Height - LedHeight - 1, ledWidth, LedHeight - 1);
+                    result[j++] = (byte)(int)res[0];
+                    result[j++] = (byte)(int)res[1];
+                    result[j++] = (byte)(int)res[2];
+                }
+
+                ledWidth = (int)(wid * metrics[3] / leds[1]);
+                offset = (int)((metrics[0] + metrics[1] + metrics[2])*wid);
+                for (int i = 0; i < leds[1]; i++)
+                {
+                    var res = AvgRect(bitmap, bounds.Width - offset - i * ledWidth, bounds.Height - LedHeight - 1, ledWidth, LedHeight - 1);
+                    result[j++] = (byte)(int)res[0];
+                    result[j++] = (byte)(int)res[1];
+                    result[j++] = (byte)(int)res[2];
+                }
+
+                ledHeight = (int)(hei * metrics[6] / leds[2]);
+                offset = (int)(metrics[5] * hei);
+                for (int i = 0; i < leds[2]; i++)
+                {
+                    var res = AvgRect(bitmap, 0, bounds.Height - offset - (i+1) * ledHeight, LedHeight - 1, ledHeight - 1);
+                    result[j++] = (byte)(int)res[0];
+                    result[j++] = (byte)(int)res[1];
+                    result[j++] = (byte)(int)res[2];
+                }
+
+                ledWidth = (int)(wid * metrics[9] / leds[1]);
+                offset = (int)(metrics[8] * wid);
+                for (int i = 0; i < leds[3]; i++)
+                {
+                    var res = AvgRect(bitmap, offset + i * ledWidth, 0, ledWidth, LedHeight);
+                    result[j++] = (byte)(int)res[0];
+                    result[j++] = (byte)(int)res[1];
+                    result[j++] = (byte)(int)res[2];
+                }
+
+
+                ledHeight = (int)(hei * metrics[12] / leds[2]);
+                offset = (int)(metrics[11] * hei);
+                for (int i = 0; i < leds[4]; i++)
+                {
+                    var res = AvgRect(bitmap, bounds.Width - LedHeight, offset + i * ledHeight, LedHeight - 1, ledHeight - 1);
+                    result[j++] = (byte)(int)res[0];
+                    result[j++] = (byte)(int)res[1];
+                    result[j++] = (byte)(int)res[2];
+                }
+            }
+            return result;
+        }
+
+        static private int[] AvgRect(Bitmap bm, int _x, int _y, int width, int height)
+        {
+            BitmapData srcData = bm.LockBits(
+            new Rectangle(0, 0, bm.Width, bm.Height),
+            ImageLockMode.ReadOnly,
+            bm.PixelFormat);
+
+            int stride =  srcData.Stride;
+            IntPtr Scan0 = srcData.Scan0;
+
+            long[] totals = new long[] { 0, 0, 0 };
+            int _height = _y + height;
+            int _width = _x + width;
+
+            unsafe
+            {
+                byte* p = (byte*)(void*)Scan0;
+                for (int y = _y; y < _height; y++)
+                {
+                    for (int x = _x; x < _width; x++)
+                    {
+                        for (int color = 0; color < 3; color++)
+                        {
+                            int idx = (y * stride) + x * 4 + color;
+                            totals[2-color] += p[idx];
+                        }
+                    }
+                }
+            }
+            bm.UnlockBits(srcData);
+            float pixelcount = width * height;
+            var tmp = new int[3] { (int)(totals[0] / pixelcount), (int)(totals[1] / pixelcount), (int)(totals[2] / pixelcount) };
+            return tmp;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            LedNumber = Int32.Parse(textBox1.Text);
+        }
+
+        private void testbutton_Click(object sender, EventArgs e)
+        {
+            panel1.Invalidate();
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            var bytes = GetBorder();
+            Rectangle bounds = Screen.GetBounds(Point.Empty);
+            using (Bitmap bitmap = new Bitmap(bounds.Width, bounds.Height))
+            {
+                using (Graphics g = Graphics.FromImage(bitmap))
+                {
+                    g.CopyFromScreen(Point.Empty, Point.Empty, bounds.Size);
+
+
+                    List<Rectangle> rectangles = new List<Rectangle>();
+
+                    double wid = bounds.Width / 64.0, hei = bounds.Height / 33.0;
+
+                    int j = 0;
+                    int ledWidth = (int)(wid * metrics[1] / leds[0]);
+                    int ledHeight = (int)(hei * metrics[6] / leds[0]);
+                    int offset = (int)(metrics[0] * wid);
+                    for (int i = 0; i < leds[0]; i++)
+                        rectangles.Add(new Rectangle(bounds.Width - offset - i * ledWidth, bounds.Height - LedHeight - 1, ledWidth, LedHeight - 1));
+
+                    ledWidth = (int)(wid * metrics[3] / leds[1]);
+                    offset = (int)((metrics[0] + metrics[1] + metrics[2] + 1) * wid);
+                    for (int i = 0; i < leds[1]; i++)
+                        rectangles.Add(new Rectangle( bounds.Width - offset - i * ledWidth, bounds.Height - LedHeight - 1, ledWidth, LedHeight - 1));
+
+                    ledHeight = (int)(hei * metrics[6] / leds[2]);
+                    offset = (int)(metrics[5] * hei);
+                    for (int i = 0; i < leds[2]; i++)
+                        rectangles.Add(new Rectangle( 0, bounds.Height - offset - (i + 1) * ledHeight, LedHeight - 1, ledHeight - 1));
+
+                    ledWidth = (int)(wid * metrics[9] / leds[1]);
+                    offset = (int)(metrics[8] * wid);
+                    for (int i = 0; i < leds[1]; i++)
+                        rectangles.Add(new Rectangle( offset + i * ledWidth, 0, ledWidth, LedHeight));
+
+                    ledHeight = (int)(hei * metrics[12] / leds[2]);
+                    offset = (int)(metrics[11] * hei);
+                    for (int i = 0; i < leds[2]; i++)
+                        rectangles.Add(new Rectangle( bounds.Width - LedHeight, offset + i * ledHeight, LedHeight - 1, ledHeight - 1));
+
+                    Pen pen = new Pen(Color.FromArgb(255,0,0,255),1);
+                    g.DrawRectangles(pen,rectangles.ToArray());
+
+                    
+                    if (drawColour)
+                    {
+                        Color customColor;
+                        SolidBrush shadowBrush;
+                        for (int i = 0; i < rectangles.Count; i++)
+                        {
+                            customColor = Color.FromArgb(bytes[i*3], bytes[i * 3+1], bytes[i * 3+2]);
+                            shadowBrush = new SolidBrush(customColor);
+                            g.FillRectangle(shadowBrush, rectangles[i]);
+                        }
+                    }
+
+                    if (drawNumber)
+                    {
+                        j = 0;
+                        foreach (var r in rectangles)
+                        {
+                            Font font = new Font(DefaultFont.FontFamily, LedHeight/2);
+                            g.DrawString($"{j++}", font, Brushes.Black, new Point(r.X + r.Width / 2 - LedHeight / 2, r.Y + r.Height / 2 - LedHeight / 2));
+                        }
+                    }
+                }
+
+                float scale = Math.Min(panel1.Width / (float)bitmap.Width, panel1.Height / (float)bitmap.Height);
+                var scaleWidth = (int)(bitmap.Width * scale);
+                var scaleHeight = (int)(bitmap.Height * scale);
+                e.Graphics.DrawImage(bitmap, ((int)panel1.Width - scaleWidth) / 2, ((int)panel1.Height - scaleHeight) / 2, scaleWidth, scaleHeight);
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            panel1.Invalidate();
+        }
+
+        private void color_CheckedChanged(object sender, EventArgs e)
+        {
+            drawNumber = !drawNumber;
+        }
+
+        private void heightled_ValueChanged(object sender, EventArgs e)
+        {
+            LedHeight = (int)heightled.Value;
+            panel1.Invalidate();
+        }
+
+        private void refresh_CheckedChanged(object sender, EventArgs e)
+        {
+            if (refresh.Checked)
+                timer1.Start();
+            else
+                timer1.Stop();
+        }
+
+        private void colour_CheckedChanged(object sender, EventArgs e)
+        {
+            drawColour = !drawColour;
+        }
+
+        private void onOffToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
